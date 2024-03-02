e44bc97d65d21454921473bb35b6cc612be2f7a7
yulianto <jooliantoo@gmail.com>
2024-03-02 20:20:43 +0700

    second commit

--- src/index.js
+++ src/index.js
@@ -1,9 +1,14 @@
 import React from "react";
+// Import ReactDOM dari module client dari package "react-dom"
 import ReactDOM from "react-dom/client";
+// Import komponen App dari file "./App"
 import App from "./App";
 
+// Mendapatkan elemen dengan id "root" dari DOM
 const element = document.getElementById("root");
 
+// Membuat root ReactDOM untuk merender elemen dengan id "root"
 const root = ReactDOM.createRoot(element);
 
+// Merender komponen App di dalam root ReactDOM yang telah dibuat sebelumnya
 root.render(<App />);
